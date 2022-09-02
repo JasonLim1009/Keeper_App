b9a620d166a27d1c9fce6a28f8efccd22fd62e22
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-09-03 01:17:06 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// 417 **********************************************
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-
